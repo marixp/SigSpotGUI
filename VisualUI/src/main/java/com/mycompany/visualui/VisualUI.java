546afde73dab3b02636851ce358d4a9f98a00122
marixp <127356431+marixp@users.noreply.github.com>
2023-04-22 17:26:20 -0700

    Add files via upload

---
+++ VisualUI/src/main/java/com/mycompany/visualui/VisualUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.visualui;
+
+/**
+ *
+ * @author moren398
+ */
+public class VisualUI {
+
+    public static void main(String[] args) {
+        new VisualGUI().setVisible(true);
+    }
+}
